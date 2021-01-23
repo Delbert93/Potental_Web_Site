022958114fddb52bdeb6c82c6879c8dff7c355fe
mckinnin <lloydmckinnin@gmail.com>
2021-01-22 23:49:16 -0700

    First commit. Made my navbar. Got help from https://www.geeksforgeeks.org/create-a-responsive-navbar-using-reactjs/

---
+++ src/pages/about.js
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import '../index.css';
+
+const About = () => { 
+return ( 
+	<div 
+	style={{ 
+		display: 'flex', 
+		justifyContent: 'Right', 
+		alignItems: 'Right', 
+		height: '100vh'
+	}} 
+	> 
+	<h1 
+        style={{
+            alignItems: 'Center'
+        }}
+    >
+        GeeksforGeeks is a Computer Science portal for geeks.
+    </h1> 
+	</div> 
+); 
+}; 
+
+export default About;
